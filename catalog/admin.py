a34a2d8853a34aecbbf32219edd27da69a4d7ec5
Ben <benlooper08@gmail.com>
2019-09-26 09:47:41 -0400

    Removed secret key from public repo, added ModelAdmin for more customizability in admin site.

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Author)
+#admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+
+#admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+#admin.site.register(BookInstance)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Language)
 
 # Register your models here.
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from secrets import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x0-hoy8lvzv^e)+5c7zl+jet^d526vs1f&mmzkt0y%#7(sqjm@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
